eed1a0799dbf7a0cea298e8d300adf4fc05d43f4
jcvillagran89 <36645501+jcvillagran89@users.noreply.github.com>
2026-03-17 13:56:47 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1>App simulando entorno real</h1>";
+
+echo "<h2>PHP funcionando</h2>";
+
+echo "<h3>Extensiones cargadas:</h3>";
+print_r(get_loaded_extensions());
+
+echo "<h3>Simulación Oracle:</h3>";
+echo getenv('LD_LIBRARY_PATH');
+
+echo "<h3>Fecha:</h3>";
+echo date("Y-m-d H:i:s");
